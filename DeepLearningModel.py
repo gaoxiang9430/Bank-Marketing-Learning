33f364de72669eee0a92617fac3b22c7f1da2175
Sandareka Wickramanayake <sandarekaw.09@cse.mrt.ac.lk>
2017-11-11 12:05:31 +0800

    modified model

--- DeepLearningModel.py
+++ DeepLearningModel.py
@@ -9,7 +9,9 @@ import csv
 from sklearn.cross_validation import train_test_split
 
 model_name = 'model'
-model_path = '/home/sandareka/Tensorflow/Data_Mining_Project' #'F:/Data Mining Project/Models' #path to save the model
+model_path_model_1 = '/home/sandareka/Tensorflow/Data_Mining_Project_1' #'F:/Data Mining Project/Models' #path to save the model
+model_path_model_2 = '/home/sandareka/Tensorflow/Data_Mining_Project_2'
+
 
 is_GPU = True
 
@@ -24,12 +26,12 @@ def csv_to_numpy_array(filePath, delimiter):
 
 def import_data():
     print("loading training and validation data")
-    dataX =  np.delete(np.genfromtxt("data_nn/pre_processed_training_data_11_10_3.csv", delimiter=","), 0, axis=0)
-    dataY = np.delete(np.genfromtxt("data_nn/training_labels_11_10_3.csv", delimiter=","), 0, axis=0)
+    dataX =  np.delete(np.genfromtxt("data_nn/pre_processed_training_data_11_10_5.csv", delimiter=","), 0, axis=0)
+    dataY = np.delete(np.genfromtxt("data_nn/training_labels_11_10_5.csv", delimiter=","), 0, axis=0)
     trainX, validationX, trainY, validationY = train_test_split(dataX, dataY, test_size=0.1, random_state=100)
 
     print("loading testing data")
-    testX = np.delete(np.genfromtxt("data_nn/pre_processed_testing_data_11_10_3.csv", delimiter=","), 0, axis=0)
+    testX = np.delete(np.genfromtxt("data_nn/pre_processed_testing_data_11_10_5.csv", delimiter=","), 0, axis=0)
     return trainX,trainY,validationX,validationY,testX
 
 #Given the label returns one hot representation of that lable
@@ -47,6 +49,26 @@ def write_output_file(output_file, output):
             output_Text = [r,output[r]]
             writer.writerow(output_Text)
 
+def draw_graphs(mean_loss_for_epoch,mcc_scores,model_name):
+
+    if len(mean_loss_for_epoch) >0:
+        plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
+        plt.rcParams['image.interpolation'] = 'nearest'
+        plt.rcParams['image.cmap'] = 'gray'
+        plt.plot(mean_loss_for_epoch, '-o')
+        plt.xlabel('epoch')
+        plt.ylabel('training loss')
+        plt.savefig('training_loss_'+model_name+'png')
+        plt.clf()
+
+    plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'
+    plt.plot(mcc_scores, '-o')
+    plt.xlabel('epoch')
+    plt.ylabel('validation mcc score')
+    plt.savefig('validation_mcc_'+model_name+'.png')
+
 trainX,train_label,validationX,validation_label,testX = import_data()
 
 trainY = convert_to_one_hot_representation(train_label)
@@ -62,14 +84,7 @@ negative_class_frequency = 0.88
 
 #200 x 4, 0.0001, 1000, 57.49
 
-# Network Parameters
-n_hidden_1 = 64#2048#512#128#2500#256 # 1st layer number of neurons
-n_hidden_2 = 64#1024#256#64#2000#256 # 2nd layer number of neurons
-n_hidden_3 = 64#512#1500#256 # 3rd layer number of neurons
-n_hidden_4 = 64#512#200 # 4th layer number of neurons
-n_hidden_5 = 64
-n_hidden_6 = 64
-n_hidden_7 = 64
+
 n_input = trainX.shape[1] # Number of features
 n_classes = 2 #Binary classification
 
@@ -82,30 +97,40 @@ with tf.device(device_name):
     initializer = tf.contrib.layers.xavier_initializer()
 
     # Store layers weight & bias
-    weights = {
-        'h1': tf.Variable(initializer([n_input, n_hidden_1])),
-        'h2': tf.Variable(initializer([n_hidden_1, n_hidden_2])),
-        'h3': tf.Variable(initializer([n_hidden_2, n_hidden_3])),
-        'h4': tf.Variable(initializer([n_hidden_3, n_hidden_4])),
-        'h5': tf.Variable(initializer([n_hidden_4, n_hidden_5])),
-        'h6': tf.Variable(initializer([n_hidden_5, n_hidden_6])),
-        'h7': tf.Variable(initializer([n_hidden_6, n_hidden_7])),
-        'out': tf.Variable(initializer([n_hidden_7, n_classes]))
-    }
-    biases = {
-        'b1': tf.Variable(tf.zeros([n_hidden_1])),
-        'b2': tf.Variable(tf.zeros([n_hidden_2])),
-        'b3': tf.Variable(tf.zeros([n_hidden_3])),
-        'b4': tf.Variable(tf.zeros([n_hidden_4])),
-        'b5': tf.Variable(tf.zeros([n_hidden_5])),
-        'b6': tf.Variable(tf.zeros([n_hidden_6])),
-        'b7': tf.Variable(tf.zeros([n_hidden_7])),
-        'out': tf.Variable(tf.zeros([n_classes]))
-    }
 
 
     # Create model
-    def multilayer_perceptron(x):
+    def multilayer_perceptron_1(x):
+        # Network Parameters
+        n_hidden_1 = 256  # 2048#512#128#2500#256 # 1st layer number of neurons
+        n_hidden_2 = 256  # 1024#256#64#2000#256 # 2nd layer number of neurons
+        n_hidden_3 = 256  # 512#1500#256 # 3rd layer number of neurons
+        n_hidden_4 = 256  # 512#200 # 4th layer number of neurons
+        n_hidden_5 = 256
+        n_hidden_6 = 256
+        n_hidden_7 = 256
+
+        weights = {
+            'h1': tf.Variable(initializer([n_input, n_hidden_1])),
+            'h2': tf.Variable(initializer([n_hidden_1, n_hidden_2])),
+            'h3': tf.Variable(initializer([n_hidden_2, n_hidden_3])),
+            'h4': tf.Variable(initializer([n_hidden_3, n_hidden_4])),
+            'h5': tf.Variable(initializer([n_hidden_4, n_hidden_5])),
+            'h6': tf.Variable(initializer([n_hidden_5, n_hidden_6])),
+            'h7': tf.Variable(initializer([n_hidden_6, n_hidden_7])),
+            'out': tf.Variable(initializer([n_hidden_7, n_classes]))
+        }
+        biases = {
+            'b1': tf.Variable(tf.zeros([n_hidden_1])),
+            'b2': tf.Variable(tf.zeros([n_hidden_2])),
+            'b3': tf.Variable(tf.zeros([n_hidden_3])),
+            'b4': tf.Variable(tf.zeros([n_hidden_4])),
+            'b5': tf.Variable(tf.zeros([n_hidden_5])),
+            'b6': tf.Variable(tf.zeros([n_hidden_6])),
+            'b7': tf.Variable(tf.zeros([n_hidden_7])),
+            'out': tf.Variable(tf.zeros([n_classes]))
+        }
+
         # Hidden fully connected layer with 256 neurons
         layer_1 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(x, weights['h1']), biases['b1'])), dropout_keep_prob)
         # Hidden fully connected layer with 256 neurons
@@ -124,35 +149,106 @@ with tf.device(device_name):
         out_layer = tf.nn.tanh(tf.add(tf.matmul(layer_7, weights['out']),biases['out']))
         return out_layer
 
+    def multilayer_perceptron_2(x):
+        # Network Parameters
+        n_hidden_1 = 128  # 2048#512#128#2500#256 # 1st layer number of neurons
+        n_hidden_2 = 128  # 1024#256#64#2000#256 # 2nd layer number of neurons
+        n_hidden_3 = 128  # 512#1500#256 # 3rd layer number of neurons
+        n_hidden_4 = 128  # 512#200 # 4th layer number of neurons
+        n_hidden_5 = 128
+        n_hidden_6 = 128
 
+        weights = {
+            'h1': tf.Variable(initializer([n_input, n_hidden_1])),
+            'h2': tf.Variable(initializer([n_hidden_1, n_hidden_2])),
+            'h3': tf.Variable(initializer([n_hidden_2, n_hidden_3])),
+            'h4': tf.Variable(initializer([n_hidden_3, n_hidden_4])),
+            'h5': tf.Variable(initializer([n_hidden_4, n_hidden_5])),
+            'h6': tf.Variable(initializer([n_hidden_5, n_hidden_6])),
+            'out': tf.Variable(initializer([n_hidden_6, n_classes]))
+        }
+        biases = {
+            'b1': tf.Variable(tf.zeros([n_hidden_1])),
+            'b2': tf.Variable(tf.zeros([n_hidden_2])),
+            'b3': tf.Variable(tf.zeros([n_hidden_3])),
+            'b4': tf.Variable(tf.zeros([n_hidden_4])),
+            'b5': tf.Variable(tf.zeros([n_hidden_5])),
+            'b6': tf.Variable(tf.zeros([n_hidden_6])),
+            'out': tf.Variable(tf.zeros([n_classes]))
+        }
+
+        # Hidden fully connected layer with 256 neurons
+        layer_1 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(x, weights['h1']), biases['b1'])), dropout_keep_prob)
+        # Hidden fully connected layer with 256 neurons
+        layer_2 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])), dropout_keep_prob)
+        # Hidden fully connected layer with 256 neurons
+        layer_3 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])), dropout_keep_prob)
+
+        layer_4 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])), dropout_keep_prob)
+
+        layer_5 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(layer_4, weights['h5']), biases['b5'])), dropout_keep_prob)
+
+        layer_6 = tf.nn.dropout(tf.nn.tanh(tf.add(tf.matmul(layer_5, weights['h6']), biases['b6'])), dropout_keep_prob)
+
+        # Output fully connected layer with a neuron for each class
+        out_layer = tf.nn.tanh(tf.add(tf.matmul(layer_6, weights['out']),biases['out']))
+        return out_layer
+
+
+"""
     # Construct model
-    logits = multilayer_perceptron(X)
+    logits_model_1 = multilayer_perceptron_1(X)
 
-    softmaxed_logits = tf.nn.softmax(logits=logits,dim=1)
-    predicted_results = tf.argmax(softmaxed_logits, 1)
+    logits_model_2 = multilayer_perceptron_2(X)
+
+    softmaxed_logits_model_1 = tf.nn.softmax(logits=logits_model_1, dim=1)
+    predicted_results_model_1 = tf.argmax(softmaxed_logits_model_1, 1)
+
+    softmaxed_logits_model_2 = tf.nn.softmax(logits=logits_model_2, dim=1)
+    predicted_results_model_2 = tf.argmax(softmaxed_logits_model_2, 1)
+
+    softmaxed_logits_ensemble_model = tf.reduce_mean([softmaxed_logits_model_1,softmaxed_logits_model_2],0)
+    predicted_results_ensemble_model = tf.argmax(softmaxed_logits_ensemble_model, 1)
 
     #predicted_results = tf.greater(softmaxed_logits, 1)
 
     # Define loss and optimizer
 
-    loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-        logits=logits, labels=Y))
+    loss_op_model_1 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        logits=logits_model_1, labels=Y))
+    loss_op_model_2 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        logits=logits_model_2, labels=Y))
+
+
     global_step = tf.Variable(0, trainable=False)
     learning_rate = tf.train.exponential_decay(learning_rate, global_step, int(trainX.shape[0] / batch_size),0.95)
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-    train_op = optimizer.minimize(loss_op)
+
+    train_op_model_1 = optimizer.minimize(loss_op_model_1)
+    train_op_model_2 = optimizer.minimize(loss_op_model_2)
     # Initializing the variables
     init = tf.global_variables_initializer()
 
+
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
 
-    saver = tf.train.Saver(max_to_keep=100)
+    saver_model_1 = tf.train.Saver(max_to_keep=100)
+    losses_model_1 = []
+    mean_loss_for_epoch_model_1 = []
+    mcc_scores_model_1 = []
+    best_mcc_model_1 = 0
+
+    saver_model_2 = tf.train.Saver(max_to_keep=100)
+    losses_model_2 = []
+    mean_loss_for_epoch_model_2 = []
+    mcc_scores_model_2 = []
+    best_mcc_model_2 = 0
+
+    mcc_scores_model_ensemble = []
+    best_mcc_model_ensemble = 0
+
     global_step = tf.Variable(0, trainable=False)
     sess.run(init)
-    losses = []
-    mean_loss_for_epoch = []
-    mcc_scores = []
-    best_mcc = 0
 
 
     # Training cycle
@@ -164,62 +260,103 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
             batch_y = trainY[index[start:end]]
             dropout_keep_prob_input = 0.5#0.5
             # Run optimization op (backprop) and cost op (to get loss value)
-            _, loss_value = sess.run([train_op, loss_op], feed_dict={X: batch_x,
+            _, loss_value_1 = sess.run([train_op_model_1, loss_op_model_1], feed_dict={X: batch_x,
                                                             Y: batch_y, dropout_keep_prob:dropout_keep_prob_input})
+
+            _, loss_value_2 = sess.run([train_op_model_2, loss_op_model_2], feed_dict={X: batch_x,
+                                                                                       Y: batch_y,
+                                                                                       dropout_keep_prob: dropout_keep_prob_input})
             # Print average loss for the batch
-            print("Current Cost: ", loss_value, "\t Epoch {}/{}".format(epoch, training_epochs),
+            print("Model 1 - Current Cost: ", loss_value_1, "\t Epoch {}/{}".format(epoch, training_epochs),
                   "\t Iter {}/{}".format(start, len(trainX)))
-            losses.append(loss_value)
+            losses_model_1.append(loss_value_1)
+
+            print("Model 2 - Current Cost: ", loss_value_2, "\t Epoch {}/{}".format(epoch, training_epochs),
+                "\t Iter {}/{}".format(start, len(trainX)))
+            losses_model_2.append(loss_value_2)
+
         #Average loss for the epoch
-        mean_loss_for_epoch.append(np.mean(losses))
-        print("Mean loss for the epoch", mean_loss_for_epoch[epoch])
+        mean_loss_for_epoch_model_1.append(np.mean(losses_model_1))
+        mean_loss_for_epoch_model_2.append(np.mean(losses_model_2))
+
+        print("Model 1-Mean loss for the epoch", mean_loss_for_epoch_model_1[epoch])
+        print("Model 2-Mean loss for the epoch", mean_loss_for_epoch_model_2[epoch])
 
         #Validation-------------------------------------
         #Predict class for the validation set and calculate MCC score
         dropout_keep_prob_input = 1.0
-        returned_predicted_results = sess.run(predicted_results, feed_dict={X: validationX, Y: validationY, dropout_keep_prob:dropout_keep_prob_input})
-        mcc_score = matthews_corrcoef(validation_label, returned_predicted_results)
-        print("MCC Score: ", mcc_score)
-        mcc_scores.append(mcc_score)
+
+        returned_predicted_results_model_1,returned_predicted_results_model_2,returned_predicted_results_ensemble = sess.run([predicted_results_model_1,predicted_results_model_2,predicted_results_ensemble_model], feed_dict={X: validationX, Y: validationY, dropout_keep_prob:dropout_keep_prob_input})
+
+        mcc_score_model_1 = matthews_corrcoef(validation_label, returned_predicted_results_model_1)
+        print("Model 1 - MCC Score: ", mcc_score_model_1)
+        mcc_scores_model_1.append(mcc_score_model_1)
 
         #Save model with the best validation results
-        if best_mcc< mcc_score:
-            best_mcc = mcc_score
-            print("Saving the model from epoch: ", epoch)
-            saver.save(sess, os.path.join(model_path, model_name), global_step=epoch)
+        if best_mcc_model_1< mcc_score_model_1:
+            best_mcc_model_1 = mcc_score_model_1
+            print("Model 1 - Saving the model from epoch: ", epoch)
+            saver_model_1.save(sess, os.path.join(model_path_model_1, model_name), global_step=epoch)
 
-    plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
-    plt.rcParams['image.interpolation'] = 'nearest'
-    plt.rcParams['image.cmap'] = 'gray'
-    plt.plot(mean_loss_for_epoch, '-o')
-    plt.xlabel('epoch')
-    plt.ylabel('training loss')
-    plt.savefig('training_loss.png')
-    plt.clf()
 
-    plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
-    plt.rcParams['image.interpolation'] = 'nearest'
-    plt.rcParams['image.cmap'] = 'gray'
-    plt.plot(mcc_scores, '-o')
-    plt.xlabel('epoch')
-    plt.ylabel('validation mcc score')
-    plt.savefig('validation_mcc.png')
+        mcc_score_model_2 = matthews_corrcoef(validation_label, returned_predicted_results_model_2)
+        print("Model 2 - MCC Score: ", mcc_score_model_2)
+        mcc_scores_model_2.append(mcc_score_model_2)
+
+        #Save model with the best validation results
+        if best_mcc_model_2< mcc_score_model_2:
+            best_mcc_model_2 = mcc_score_model_2
+            print("Model 2 - Saving the model from epoch: ", epoch)
+            saver_model_2.save(sess, os.path.join(model_path_model_2, model_name), global_step=epoch)
+
+        mcc_score_model_ensemble = matthews_corrcoef(validation_label, returned_predicted_results_ensemble)
+        print("Ensemble model - MCC Score: ", mcc_score_model_ensemble)
+        mcc_scores_model_ensemble.append(mcc_score_model_ensemble)
+
+        #Save model with the best validation results
+        if best_mcc_model_ensemble< mcc_score_model_ensemble:
+            best_mcc_model_ensemble = mcc_score_model_ensemble
+
+    draw_graphs(mean_loss_for_epoch_model_1,mcc_score_model_1,"model_1")
+    draw_graphs(mean_loss_for_epoch_model_2, mcc_score_model_2, "model_2")
+    draw_graphs([], mcc_scores_model_ensemble, "model_ensemble")
+
 
     print("Optimization Finished!")
-    print("Best MCC Score Achieved",best_mcc)
+    print("Model 1 Best MCC Score Achieved", best_mcc_model_1)
+    print("Model 2 Best MCC Score Achieved", best_mcc_model_2)
+    print("Ensemble Model Best MCC Score Achieved", best_mcc_model_ensemble)
 
 """
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
     # Test model
+    logits_model_1 = multilayer_perceptron_1(X)
     saver = tf.train.Saver()
-    saved_path = tf.train.latest_checkpoint(model_path)
+    saved_path = tf.train.latest_checkpoint(model_path_model_1)
     saver.restore(sess, saved_path)
     dropout_keep_prob_input = 1.0
-    logits = multilayer_perceptron(X)
-    softmaxed_logits = tf.nn.softmax(logits=logits, dim=1)
-    weighted_results = softmaxed_logits
-    predicted_results = tf.argmax(softmaxed_logits, 1)
-    returned_predicted_results = sess.run(predicted_results, feed_dict={X: testX,dropout_keep_prob: dropout_keep_prob_input})
-    write_output_file("results_11_10_4.csv",returned_predicted_results)
-"""
+    softmaxed_logits_model_1 = tf.nn.softmax(logits=logits_model_1, dim=1)
+
+    #predicted_results_model_1 = tf.argmax(softmaxed_logits_model_1, 1)
+    #returned_predicted_results = sess.run(predicted_results_model_1, feed_dict={X: testX, dropout_keep_prob: dropout_keep_prob_input})
+    returned_softmaxed_logits_model_1 = sess.run(softmaxed_logits_model_1,feed_dict={X: testX, dropout_keep_prob: dropout_keep_prob_input})
+
+with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
+    # Test model
+    logits_model_2 = multilayer_perceptron_2(X)
+    saver_model_2 = tf.train.Saver()
+    saved_path_model_2 = tf.train.latest_checkpoint(model_path_model_2)
+    saver_model_2.restore(session, saved_path_model_2)
+    dropout_keep_prob_input = 1.0
+    softmaxed_logits_model_2 = tf.nn.softmax(logits=logits_model_2, dim=1)
+
+    # predicted_results_model_1 = tf.argmax(softmaxed_logits_model_1, 1)
+    # returned_predicted_results = sess.run(predicted_results_model_1, feed_dict={X: testX, dropout_keep_prob: dropout_keep_prob_input})
+
+    returned_softmaxed_logits_model_2 = session.run(softmaxed_logits_model_2,feed_dict={X: testX, dropout_keep_prob: dropout_keep_prob_input})
+
+softmaxed_logits_ensemble_model = np.mean([returned_softmaxed_logits_model_1, returned_softmaxed_logits_model_2], 0)
+predicted_results_ensemble_model = np.argmax(softmaxed_logits_ensemble_model, 1)
+write_output_file("results_11_10_6.csv",predicted_results_ensemble_model)
+
 
