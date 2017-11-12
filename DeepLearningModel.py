c45d15edaa2a940e080d71810704e199bec5c10a
Sandareka Wickramanayake <sandarekaw.09@cse.mrt.ac.lk>
2017-11-12 22:04:06 +0800

    Adding final code files

--- DeepLearningModel.py
+++ DeepLearningModel.py
@@ -9,8 +9,11 @@ import csv
 from sklearn.cross_validation import train_test_split
 
 model_name = 'model'
-model_path_model_1 = '/home/sandareka/Tensorflow/Data_Mining_Project_1' #'F:/Data Mining Project/Models' #path to save the model
-model_path_model_2 = '/home/sandareka/Tensorflow/Data_Mining_Project_2'
+#model_path_model_1 = '/home/sandareka/Tensorflow/Data_Mining_Project_1' #'F:/Data Mining Project/Models' #path to save the model
+#model_path_model_2 = '/home/sandareka/Tensorflow/Data_Mining_Project_2'
+
+model_path_model_1 = '/home/sandareka/Tensorflow/Data_Mining_Project_3' #'F:/Data Mining Project/Models' #path to save the model
+model_path_model_2 = '/home/sandareka/Tensorflow/Data_Mining_Project_4'
 
 
 is_GPU = True
@@ -26,12 +29,17 @@ def csv_to_numpy_array(filePath, delimiter):
 
 def import_data():
     print("loading training and validation data")
-    dataX =  np.delete(np.genfromtxt("data_nn/pre_processed_training_data_11_10_5.csv", delimiter=","), 0, axis=0)
-    dataY = np.delete(np.genfromtxt("data_nn/training_labels_11_10_5.csv", delimiter=","), 0, axis=0)
-    trainX, validationX, trainY, validationY = train_test_split(dataX, dataY, test_size=0.1, random_state=100)
+    trainX =  np.delete(np.genfromtxt("data_nn/pre_processed_training_data_11_12_6.csv", delimiter=","), 0, axis=0)
+    trainY = np.delete(np.genfromtxt("data_nn/training_labels_11_12_6.csv", delimiter=","), 0, axis=0)
+
+    #Loading data for validation
+    validX =  np.delete(np.genfromtxt("data_nn/pre_processed_validation_data_11_12_6.csv", delimiter=","), 0, axis=0)
+    validY = np.delete(np.genfromtxt("data_nn/validation_labels_11_12_6.csv", delimiter=","), 0, axis=0)
+
+    trainX_, validationX, trainY_, validationY = train_test_split(validX, validY, test_size=0.1, random_state=100)
 
     print("loading testing data")
-    testX = np.delete(np.genfromtxt("data_nn/pre_processed_testing_data_11_10_5.csv", delimiter=","), 0, axis=0)
+    testX = np.delete(np.genfromtxt("data_nn/pre_processed_testing_data_11_12_6.csv", delimiter=","), 0, axis=0)
     return trainX,trainY,validationX,validationY,testX
 
 #Given the label returns one hot representation of that lable
@@ -68,6 +76,31 @@ def draw_graphs(mean_loss_for_epoch,mcc_scores,model_name):
     plt.xlabel('epoch')
     plt.ylabel('validation mcc score')
     plt.savefig('validation_mcc_'+model_name+'.png')
+    plt.clf()
+
+def draw_graphs_mcc(model1,model2,ensemble_model):
+
+    """
+    plt.subplot(2, 1, 2)
+    plt.plot(solver.train_acc_history, '-o')
+    plt.plot(solver.val_acc_history, '-o')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.show()
+    """
+
+    plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'
+    plt.plot(model1, '-o')
+    plt.plot(model2, '-o')
+    plt.plot(ensemble_model, '-o')
+    plt.xlabel('epoch')
+    plt.ylabel('validation mcc score')
+    plt.legend(['Model 1', 'Model 2','Ensemble model'], loc='upper left')
+    plt.savefig('validation_mcc_comparison.png')
+    plt.clf()
 
 trainX,train_label,validationX,validation_label,testX = import_data()
 
@@ -92,6 +125,7 @@ with tf.device(device_name):
     # tf Graph input
     X = tf.placeholder("float", [None, n_input])
     Y = tf.placeholder("float", [None, n_classes])
+    given_labels = tf.placeholder("float", [None])
     dropout_keep_prob = tf.placeholder(tf.float32)
 
     initializer = tf.contrib.layers.xavier_initializer()
@@ -195,7 +229,8 @@ with tf.device(device_name):
         return out_layer
 
 
-"""
+
+
     # Construct model
     logits_model_1 = multilayer_perceptron_1(X)
 
@@ -209,6 +244,10 @@ with tf.device(device_name):
 
     softmaxed_logits_ensemble_model = tf.reduce_mean([softmaxed_logits_model_1,softmaxed_logits_model_2],0)
     predicted_results_ensemble_model = tf.argmax(softmaxed_logits_ensemble_model, 1)
+
+    auc_score_1 = tf.metrics.auc(given_labels, predicted_results_model_1)
+    auc_score_2 = tf.metrics.auc(given_labels, predicted_results_model_2)
+    auc_score_ensemble = tf.metrics.auc(given_labels, predicted_results_ensemble_model)
 
     #predicted_results = tf.greater(softmaxed_logits, 1)
 
@@ -226,8 +265,10 @@ with tf.device(device_name):
 
     train_op_model_1 = optimizer.minimize(loss_op_model_1)
     train_op_model_2 = optimizer.minimize(loss_op_model_2)
+
+
     # Initializing the variables
-    init = tf.global_variables_initializer()
+    init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
 
 
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -237,15 +278,21 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
     mean_loss_for_epoch_model_1 = []
     mcc_scores_model_1 = []
     best_mcc_model_1 = 0
+    auc_scores_model_1 = []
+    best_auc_model_1 = 0
 
     saver_model_2 = tf.train.Saver(max_to_keep=100)
     losses_model_2 = []
     mean_loss_for_epoch_model_2 = []
     mcc_scores_model_2 = []
     best_mcc_model_2 = 0
+    auc_scores_model_2 = []
+    best_auc_model_2 = 0
 
     mcc_scores_model_ensemble = []
     best_mcc_model_ensemble = 0
+    auc_scores_ensemble = []
+    best_auc_ensemble = 0
 
     global_step = tf.Variable(0, trainable=False)
     sess.run(init)
@@ -258,14 +305,16 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         for start, end in zip(range(0, len(index), batch_size), range(batch_size, len(index), batch_size)):
             batch_x = trainX[index[start:end]]
             batch_y = trainY[index[start:end]]
-            dropout_keep_prob_input = 0.5#0.5
+            batch_lable = train_label[index[start:end]]
+            dropout_keep_prob_input = 1.0#0.5
             # Run optimization op (backprop) and cost op (to get loss value)
             _, loss_value_1 = sess.run([train_op_model_1, loss_op_model_1], feed_dict={X: batch_x,
-                                                            Y: batch_y, dropout_keep_prob:dropout_keep_prob_input})
+                                                            Y: batch_y, dropout_keep_prob:dropout_keep_prob_input,given_labels:batch_lable})
 
             _, loss_value_2 = sess.run([train_op_model_2, loss_op_model_2], feed_dict={X: batch_x,
                                                                                        Y: batch_y,
-                                                                                       dropout_keep_prob: dropout_keep_prob_input})
+                                                                                       dropout_keep_prob: dropout_keep_prob_input,
+                                                                                       given_labels:batch_lable})
             # Print average loss for the batch
             print("Model 1 - Current Cost: ", loss_value_1, "\t Epoch {}/{}".format(epoch, training_epochs),
                   "\t Iter {}/{}".format(start, len(trainX)))
@@ -286,11 +335,15 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         #Predict class for the validation set and calculate MCC score
         dropout_keep_prob_input = 1.0
 
-        returned_predicted_results_model_1,returned_predicted_results_model_2,returned_predicted_results_ensemble = sess.run([predicted_results_model_1,predicted_results_model_2,predicted_results_ensemble_model], feed_dict={X: validationX, Y: validationY, dropout_keep_prob:dropout_keep_prob_input})
+        returned_predicted_results_model_1,returned_predicted_results_model_2,returned_predicted_results_ensemble, returned_auc_score_1,returned_auc_score_2, returned_auc_score_ensemble = sess.run([predicted_results_model_1,predicted_results_model_2,predicted_results_ensemble_model,auc_score_1,auc_score_2,auc_score_ensemble], feed_dict={X: validationX, Y: validationY, dropout_keep_prob:dropout_keep_prob_input,given_labels:validation_label})
 
         mcc_score_model_1 = matthews_corrcoef(validation_label, returned_predicted_results_model_1)
         print("Model 1 - MCC Score: ", mcc_score_model_1)
         mcc_scores_model_1.append(mcc_score_model_1)
+
+
+        if best_auc_model_1 < returned_auc_score_1[0]:
+            best_auc_model_1 = returned_auc_score_1[0]
 
         #Save model with the best validation results
         if best_mcc_model_1< mcc_score_model_1:
@@ -303,6 +356,10 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         print("Model 2 - MCC Score: ", mcc_score_model_2)
         mcc_scores_model_2.append(mcc_score_model_2)
 
+
+        if best_auc_model_2 < returned_auc_score_2[0]:
+            best_auc_model_2 = returned_auc_score_2[0]
+
         #Save model with the best validation results
         if best_mcc_model_2< mcc_score_model_2:
             best_mcc_model_2 = mcc_score_model_2
@@ -313,19 +370,29 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         print("Ensemble model - MCC Score: ", mcc_score_model_ensemble)
         mcc_scores_model_ensemble.append(mcc_score_model_ensemble)
 
-        #Save model with the best validation results
-        if best_mcc_model_ensemble< mcc_score_model_ensemble:
+        if best_mcc_model_ensemble < mcc_score_model_ensemble:
             best_mcc_model_ensemble = mcc_score_model_ensemble
 
-    draw_graphs(mean_loss_for_epoch_model_1,mcc_score_model_1,"model_1")
-    draw_graphs(mean_loss_for_epoch_model_2, mcc_score_model_2, "model_2")
+        #Save model with the best validation results
+        if best_auc_ensemble< returned_auc_score_ensemble [0]:
+            best_auc_ensemble = returned_auc_score_ensemble[0]
+
+    draw_graphs_mcc(mcc_scores_model_1, mcc_scores_model_2, mcc_scores_model_ensemble)
+
+    draw_graphs(mean_loss_for_epoch_model_1,mcc_scores_model_1,"model_1")
+    draw_graphs(mean_loss_for_epoch_model_2, mcc_scores_model_2, "model_2")
     draw_graphs([], mcc_scores_model_ensemble, "model_ensemble")
+
+
 
 
     print("Optimization Finished!")
     print("Model 1 Best MCC Score Achieved", best_mcc_model_1)
+    print("Model2 Best AUC", best_auc_model_1)
     print("Model 2 Best MCC Score Achieved", best_mcc_model_2)
+    print("Model2 Best AUC", best_auc_model_2)
     print("Ensemble Model Best MCC Score Achieved", best_mcc_model_ensemble)
+    print("Ensemble Model Best AUC", best_auc_ensemble)
 
 """
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
@@ -339,7 +406,12 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
 
     #predicted_results_model_1 = tf.argmax(softmaxed_logits_model_1, 1)
     #returned_predicted_results = sess.run(predicted_results_model_1, feed_dict={X: testX, dropout_keep_prob: dropout_keep_prob_input})
+    test_results_model_1 = []
+    n_test_input = testX.shape[0]
+    
+    #for i in range (n_test_input):
     returned_softmaxed_logits_model_1 = sess.run(softmaxed_logits_model_1,feed_dict={X: testX, dropout_keep_prob: dropout_keep_prob_input})
+
 
 with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
     # Test model
@@ -357,6 +429,5 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
 
 softmaxed_logits_ensemble_model = np.mean([returned_softmaxed_logits_model_1, returned_softmaxed_logits_model_2], 0)
 predicted_results_ensemble_model = np.argmax(softmaxed_logits_ensemble_model, 1)
-write_output_file("results_11_10_6.csv",predicted_results_ensemble_model)
-
-
+write_output_file("results_11_12_4.csv",predicted_results_ensemble_model)
+"""
--- New_NN_Preprocessing.R
+++ New_NN_Preprocessing.R
@@ -1,4 +1,6 @@
 library(ROSE) #Helps to generate artificial data based on sampling methods and smoothed bootstrap approach
+library(plyr)
+
 
 setwd("F:/OneDrive/OneDrive - National University of Singapore/Data Mining My Project/Data-Mining-1")
 training_data <- read.csv(file="data/train.csv",head=TRUE,sep=",") #Load training data
@@ -7,8 +9,26 @@ print("Data read....!")
 
 #Selecting more important variables for pre-processing
 training_data <- training_data[,c("age", "job", "education", "month", "day_of_week", "duration", "campaign", "pdays", "poutcome", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed","y")]
-testing_data <- testing_data[,c("age", "job", "education", "month", "day_of_week", "duration", "campaign", "pdays", "poutcome", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed")]
+validation_data<- training_data[,c("age", "job", "education","month", "day_of_week", "duration", "campaign", "pdays",  "poutcome", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed","y")]
+testing_data <- testing_data[,c("age", "job", "education",  "month", "day_of_week", "duration", "campaign", "pdays", "poutcome", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed")]
 print("Features selected...!")
+
+#Removing rows with "unknown" values
+#training_data <-subset(training_data, education!="unknown")
+#testing_data <-subset(testing_data, education!="unknown")
+#validation_data <- subset(validation_data, education!="unknown")
+
+#training_data <-subset(training_data, job!="unknown")
+#testing_data <-subset(testing_data, job!="unknown")
+#validation_data <- subset(validation_data, job!="unknown")
+
+training_data$education <- revalue(training_data$education, c("unknown"="university.degree")) 
+testing_data$education <-revalue(testing_data$education, c("unknown"="university.degree")) 
+validation_data$education <- revalue(validation_data$education, c("unknown"="university.degree")) 
+
+training_data$job <-revalue(training_data$job, c("unknown"="admin."))
+testing_data$job <-revalue(testing_data$job, c("unknown"="admin."))
+validation_data$job <- revalue(validation_data$job, c("unknown"="admin."))
 
 #Addressing data inbalance
 #Do both oversampling and undersampling to create a balanced dataset but can cause to lose some information and there by inaccuries
@@ -16,9 +36,18 @@ print("Features selected...!")
 
 #The ROSE pacakge addresses the above problem and provides balanced and informative dataset
 balanced_training_data <- ROSE(y ~ ., data = training_data, seed = 1)$data
+#balanced_training_data <- ovun.sample(y ~ ., data = training_data, method = "both", p=0.5, N=30891, seed = 1)$data
+
+
 #Extract training labels
 balanced_training_data <- transform(balanced_training_data, y = ifelse(y == "yes", 1, 0))
 balanced_training_labels <-balanced_training_data$y
+
+#For validation
+validation_data <- transform(validation_data, y = ifelse(y == "yes", 1, 0))
+validation_labels <- validation_data$y
+
+
 #Prints class distribution
 print("Data set is now balanced...!")
 table(balanced_training_data$y)
@@ -33,11 +62,24 @@ df2 <- data.frame(model.matrix(~education-1,balanced_training_features))
 df3 <- data.frame(model.matrix(~month-1,balanced_training_features))
 df4 <- data.frame(model.matrix(~day_of_week-1,balanced_training_features))
 df5 <- data.frame(model.matrix(~poutcome-1,balanced_training_features))
+#df6 <- data.frame(model.matrix(~housing-1,balanced_training_features))
+
+vdf1 <- data.frame(model.matrix(~job-1,validation_data))
+vdf2 <- data.frame(model.matrix(~education-1,validation_data))
+vdf3 <- data.frame(model.matrix(~month-1,validation_data))
+vdf4 <- data.frame(model.matrix(~day_of_week-1,validation_data))
+vdf5 <- data.frame(model.matrix(~poutcome-1,validation_data))
+#vdf6 <- data.frame(model.matrix(~housing-1,validation_data))
 
 print("Categorical variables are converted to their one-hot mapping...!")
 categorical_features_training <- cbind(df1, df2,df3,df4,df5)
 continous_variables_training <- balanced_training_features[,c("age", "duration", "campaign", "pdays", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed")]
 preprocessed_data <- cbind(continous_variables_training, categorical_features_training)
+
+#For validation dataset
+v_categorical_features_training <- cbind(vdf1, vdf2,vdf3,vdf4,vdf5)
+v_continous_variables_training <- validation_data[,c("age", "duration", "campaign", "pdays",  "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed")]
+v_preprocessed_data <- cbind(v_continous_variables_training, v_categorical_features_training)
 
 
 #----Following code snippt is not using----------
@@ -49,14 +91,21 @@ balanced_training_features$poutcome <-as.numeric(balanced_training_features$pout
 
 scaled_training <- scale(preprocessed_data,center = TRUE,scale = TRUE)
 #scaled_training <- scale(balanced_training_features,center = TRUE,scale = TRUE)
+
+#for validation data
+scaled_validation <- scale(v_preprocessed_data,center = TRUE,scale = TRUE)
+
 print("Data set is normalized...!")
 
 final_training_data <- scaled_training[0:30891,]
 final_testing_data <- scaled_training[30892:41188,]
 
 
-write.csv(file="pre_processed_training_data_11_10_5.csv", x=final_training_data,row.names = FALSE) 
-write.csv(file="training_labels_11_10_5.csv", x=balanced_training_labels,row.names = FALSE)
-write.csv(file="pre_processed_testing_data_11_10_5.csv", x=final_testing_data,row.names = FALSE) 
+write.csv(file="pre_processed_training_data_11_12_6.csv", x=final_training_data,row.names = FALSE) 
+write.csv(file="training_labels_11_12_6.csv", x=balanced_training_labels,row.names = FALSE)
+write.csv(file="pre_processed_testing_data_11_12_6.csv", x=final_testing_data,row.names = FALSE) 
+
+write.csv(file="pre_processed_validation_data_11_12_6.csv", x=scaled_validation,row.names = FALSE) 
+write.csv(file="validation_labels_11_12_6.csv", x=validation_labels,row.names = FALSE)
 
 print("Preprocessed files are generated....!")
